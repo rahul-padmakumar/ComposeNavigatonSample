4ab6160a7f2352e15e01fb14a2ca7aac7987c0e1
rahul <rahulp5875@gmail.com>
2024-06-21 14:14:03 +0530

    Code clean up.

--- app/src/main/java/com/example/composenavigationsample/MainActivity.kt
+++ app/src/main/java/com/example/composenavigationsample/MainActivity.kt
@@ -6,15 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
